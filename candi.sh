a3441d2c66f61b2a747c383581da19c08c42f563
Timo Heister <timo.heister@gmail.com>
2015-10-03 09:44:59 -0400

    no python error

--- candi.sh
+++ candi.sh
@@ -641,7 +641,7 @@ fi
 
 # If the platform doesn't override the system python by installing its
 # own, figure out the version of the existing python
-default PYTHONVER=`python -c "import sys; print sys.version[:3]"`
+default PYTHONVER=`python -c "import sys; print sys.version[:3]" 2>/dev/null || echo -1`
 
 # Create necessary directories and set appropriate variables
 mkdir -p ${DOWNLOAD_PATH}
