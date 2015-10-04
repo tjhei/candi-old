c8ba5345727b8a2a07ddf124851428d8b3c8e7f2
Timo Heister <timo.heister@gmail.com>
2015-10-04 12:00:43 -0400

    setup forwards commands

--- _setup.sh
+++ _setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export PREFIX_PATH=`pwd`/out
 cd "$(dirname "$0")"
-PROCS=8 CC=mpicc CXX=mpicxx FC=mpif90 FF=mpif77 ./candi.sh
+PROCS=6 CC=mpicc CXX=mpicxx FC=mpif90 FF=mpif77 ./candi.sh "$@"
