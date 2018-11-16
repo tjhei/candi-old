48c518ca7be3751be6e4baea5c0d251e5ae9ae11
Timo Heister <timo.heister@gmail.com>
2018-11-16 14:57:00 -0500

    update versions

--- make.sh
+++ make.sh
@@ -4,13 +4,15 @@ set -e
 docker build -t tjhei/candi-base base/
 
 cd v85; ./make.sh v8.5.1-r1; cd ..
-cd v90; ./make.sh v9.0.1-r3; cd ..
+cd v90; ./make.sh v9.0.1-r4; cd ..
 
 cd dealii; ./make.sh v8.5.1 v8.5.1-r1; cd ..
-cd dealii; ./make.sh v9.0.1 v9.0.1-r3; cd ..
-cd dealii; ./make.sh master v9.0.1-r3; cd ..
+cd dealii; ./make.sh v9.0.1 v9.0.1-r4; cd ..
+
+master=e2c9db2
+#cd dealii; ./make.sh $master v9.0.1-r4; cd ..
 
 docker push tjhei/candi:v8.5.1-r1
-docker push tjhei/candi:v9.0.1-r3
+docker push tjhei/candi:v9.0.1-r4
 docker push tjhei/dealii:v8.5.1-full-v8.5.1-r1
-docker push tjhei/dealii:v9.0.1-full-v9.0.1-r3
+docker push tjhei/dealii:v9.0.1-full-v9.0.1-r4
