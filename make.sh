88a1bd8e26d48c3c3ad7f801277e51f485c6f036
Timo Heister <timo.heister@gmail.com>
2020-07-01 15:36:06 -0400

    add deal.II 9.2

--- make.sh
+++ make.sh
@@ -3,6 +3,7 @@ set -e
 set -o xtrace
 
 ## base:
+docker build -t tjhei/candi-base:base base/
 docker build -t tjhei/candi-base:gcc5 base-gcc5/
 docker build -t tjhei/candi-base:clang6 base-clang6/
 docker build -t tjhei/candi-base:gcc5-openmpi base-gcc5-openmpi/
@@ -29,12 +30,15 @@ docker build -t tjhei/candi-base:gcc5-openmpi base-gcc5-openmpi/
 # cd dealii; ./make.sh v9.1.1 v9.1.1-r2 gcc5-openmpi; cd ..
 # TODO cd dealii; ./make.sh v9.1.1 v9.1.1-r1 clang6; cd ..
 
+## 9.2:
+cd v92; ./make.sh v9.2.0-r1 gcc5; cd ..
+#cd v92; ./make.sh v9.2.0-r1 clang6; cd ..
+cd dealii; ./make.sh v9.2.0 v9.2.0-r1 gcc5; cd ..
 
 
 #master=e2c9db2
 #cd dealii; ./make.sh $master v9.0.1-r5; cd ..
 
-TODO: make 9.2.0-r1 with new trilinos 
 pause
 
 
---
+++ v92/make.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+set -e
+
+if [ $# -eq 1 ]; then
+    TAG=$1
+    BASEVER=gcc5
+elif [ $# -eq 2 ]; then
+    TAG=$1
+    BASEVER=$2
+else
+    echo "Usage: $0 TAG BASEVER"
+    echo "  where TAG is one of:"
+    git tag -l 'v9.0.*'
+    echo "  where BASEVER is one of the versions of:"
+    docker images -f "reference=tjhei/candi-base"
+    exit 1
+fi
+
+REPO=tjhei
+
+path=$PWD
+rm -rf candi && cd .. && git archive --format=tar --prefix=candi/ $TAG | (cd $path; tar xf -)
+cd $path
+
+from=tjhei/candi-base:$BASEVER
+out=tjhei/candi:$TAG-$BASEVER
+echo "building $out from $from"
+sed "s!%%FROM%%!$from!g" Dockerfile.in >Dockerfile.tmp
+docker build -t $out -f Dockerfile.tmp .
+echo "built $out"
